dfe0c3cd62bdcfc881685923d2ac105c11950b94
Niilo Keinänen <niilo.keinanen@lightningchart.com>
2025-08-06 11:11:32 +0300

    LCJS v8.0.0

--- src/index.js
+++ src/index.js
@@ -83,23 +83,13 @@ const gasoline = [
 ]
 
 // Add two line series.
-const lineSeries = chart.addPointLineAreaSeries({ dataPattern: 'ProgressiveX' }).setAreaFillStyle(emptyFill).setName('Diesel')
-const lineSeries2 = chart.addPointLineAreaSeries({ dataPattern: 'ProgressiveX' }).setAreaFillStyle(emptyFill).setName('Gasoline')
+const lineSeries = chart.addLineSeries().setName('Diesel')
+const lineSeries2 = chart.addLineSeries().setName('Gasoline')
 
 // Add the points to each Series
-lineSeries2.add(diesel)
-lineSeries.add(gasoline)
+lineSeries2.appendJSON(diesel)
+lineSeries.appendJSON(gasoline)
 
 // Setup view nicely.
 chart.axisY.setTitle('Price').setUnits('$/litre').setInterval({ start: 0, end: 3, stopAxisAfter: true })
 
-const legend = chart
-    .addLegendBox()
-    // Dispose example UI elements automatically if they take too much space. This is to avoid bad UI on mobile / etc. devices.
-    .setAutoDispose({
-        type: 'max-width',
-        maxWidth: 0.3,
-    })
-
-// Add Chart to LegendBox.
-legend.add(chart)
